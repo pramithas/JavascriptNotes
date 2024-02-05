3f405f0e951c16fa73382dd451c7de055a4c4dc2
Pramithas Dhakal <pramithas.dhakal@renegadeinsurance.com>
2024-02-05 09:23:35 +0545

    Add example for datatypes

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+x  // primitive data types
+let d = BigInt("123") + BigInt("123");
+let a = undefined;
+console.log(d,a)
+console.log(typeof d)
+
+// non primitive data type ie object
+
+const employee = {
+  "name":"Pramithas",
+  "title":"Software Engineer"
+};
+
+console.log(employee.name)
