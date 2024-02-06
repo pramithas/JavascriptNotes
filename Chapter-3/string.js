2b8c22c141e25c9135dbfcb2d9409b8c2981cc56
Pramithas Dhakal <pramithas.dhakal@renegadeinsurance.com>
2024-02-06 08:38:29 +0545

    Add string notes

---
+++ Chapter-3/string.js
@@ -0,0 +1,16 @@
+let name = "John"; 
+console.log(name.length);
+console.log(name[0]);
+
+// template literals
+let boy1 = "Pramod";
+let boy2 = "Nikhil";
+
+let sentence = `${boy1} is a friend of ${boy2}`;
+console.log(sentence);
+
+// escape characters
+let fruit = 'Bana\'na';
+console.log(fruit);
+console.log(fruit.length)
+
