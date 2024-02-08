4ab83f9ca54c165eb9fa674a2deb60eacb5516e1
Pramithas Dhakal <dhakalpramithas@gmail.com>
2024-02-08 11:10:37 +0545

    Add table navigation

---
+++ Chapter-7/Element Only Navigation/index.js
@@ -0,0 +1,3 @@
+let b = document.body
+console.log("First child of b is:", b.firstChild);
+console.log("First element child is: ", b.firstElementChild);
---
+++ Chapter-7/Table Navigation/index.js
@@ -0,0 +1,10 @@
+let t = document.body.firstElementChild.firstElementChild;
+
+console.log(t);
+
+console.log(t.rows);
+console.log(t.tHead.firstElementChild);
+console.log(t.tFoot);
+console.log(t.tBodies); 
+//find row index
+console.log(t.rows[0].rowIndex)
